9de86793ec44a9a6a085d117993ff6fb6002ebd5
Vipul Munot <vipmunot@gmail.com>
2017-04-03 13:45:15 -0400

    adding search page

---
+++ search.php
@@ -0,0 +1,85 @@
+<?php
+include 'header.php';
+echo "
+<div class='clearfix'>&nbsp;</div>
+<section id = 'categories'>
+<div class='col-md-4'></div>
+<div class = 'container'>
+<div class='clearfix'>&nbsp;</div>
+<div class = 'row'><h2 class='text-center'> What is on your mind? </h2></div>
+<div class='clearfix'>&nbsp;</div>
+<div class ='row'>
+<div class='col-md-4'>Search Options</div>
+<div class='col-md-8'>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>
+
+</div><!--End col-md-8-->
+</div><!--End row-->
+</div><!--End container-->
+
+</section>";
+include 'footer.php';
+
+?>
