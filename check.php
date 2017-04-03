425c9da6203157aaafe3a5583f94e5ea53bac629
Vipul Munot <vipmunot@gmail.com>
2017-04-03 13:08:20 -0400

    separating header and footer into php files

--- check.php
+++ check.php
@@ -1,15 +1,30 @@
 <?php
-$servername = "mysql.000webhost.com";
-$username = "vipulmun_rohit";
-$password = "rohit123";
-$database = "vipulmun_friends";
+  $servername = "eventpal.cp4hghmjwcmi.us-west-2.rds.amazonaws.com";
+  $username = "rohit";
+  $password = "rohitnair987";
+  $database = "eventpal";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password, $database);
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+  // Check connection
+  if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+  echo "Connected successfully";
+
+$query = "SELECT Name FROM Interest;";
+mysqli_query($conn, $query) or die('Error querying the database.');
+
+$result = mysqli_query($conn, $query);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["Name"]. "<br>";
+    }
+} else {
+    echo "0 results";
 }
-echo "Connected successfully";
+
+mysqli_close($conn);
 ?>
---
+++ index.php
@@ -0,0 +1,188 @@
+<?php
+include 'header.php';
+echo"<section id='slider'>
+    <div id='myCarousel' class='carousel slide' data-interval='2000' data-ride='carousel'>
+    	<!-- Carousel indicators -->
+        <ol class='carousel-indicators'>
+            <li data-target='#myCarousel' data-slide-to='0' class='active'></li>
+            <li data-target='#myCarousel' data-slide-to='1'></li>
+            <li data-target='#myCarousel' data-slide-to='2'></li>
+        </ol>   
+        <!-- Wrapper for carousel items -->
+        <div class='carousel-inner'>
+            <div class='active item'>
+                <img class = 'fill' src='images/600.jpg' alt='First Slide'>
+         		<div class='carousel-caption'>
+                  <h3>First slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class='item'>
+                <img src='images/600.jpg' alt='Second Slide'>
+                <div class='carousel-caption'>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <a href='http://bootstrapthemes.co/' target='_blank'  class='btn btn-primary' data-animation='animated fadeInDown'>Sign up</a>
+                </div>
+            </div>
+            <div class='item'>
+                <img src='images/600.jpg' alt='Third Slide'>
+                <div class='carousel-caption'>
+                  <h3>Third slide label</h3>
+                  <a href='http://bootstrapthemes.co/' target='_blank'  class='btn btn-primary' data-animation='animated fadeInDown'>select two</a>
+                </div>
+            </div>
+        </div>
+        <!-- Carousel controls -->
+        <a class='carousel-control left' href='#myCarousel' data-slide='prev'>
+            <span class='glyphicon glyphicon-chevron-left'></span>
+        </a>
+        <a class='carousel-control right' href='#myCarousel' data-slide='next'>
+            <span class='glyphicon glyphicon-chevron-right'></span>
+        </a>
+    </div>    
+
+</section>
+<section id = 'upcomingevents'></section>
+<section id = 'categories'>
+	<div class='container'>
+	<div class='clearfix'>&nbsp;</div>
+	<div class='row'> <h2 class='text-center'> What is on your mind? </h2></div>
+	<div class='clearfix'>&nbsp;</div>
+<div class='row'>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>			
+</div><!--End row-->
+<div class='row'>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>			
+</div><!--End row-->
+<div class='row'>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>
+<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+<h3>Adventure</h3>
+
+<p>Come join the adventures with us</p>
+<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+</div>			
+</div><!--End row-->
+	</div>
+</section>
+<section id = 'testmonials'>
+<div class='carousel'>
+		<div class='container'>
+		<div class='row'>
+			<div class='col-md-8 col-md-offset-2'>
+                <div class='quote'><i class='fa fa-quote-left fa-4x'></i></div>
+				<div class='carousel slide' id='fade-quote-carousel' data-ride='carousel' data-interval='3000'>
+				  <!-- Carousel indicators -->
+                  <ol class='carousel-indicators'>
+				    <li data-target='#fade-quote-carousel' data-slide-to='0'></li>
+				    <li data-target='#fade-quote-carousel' data-slide-to='1'></li>
+				    <li data-target='#fade-quote-carousel' data-slide-to='2' class='active'></li>
+                    <li data-target='#fade-quote-carousel' data-slide-to='3'></li>
+                    <li data-target='#fade-quote-carousel' data-slide-to='4'></li>
+                    <li data-target='#fade-quote-carousel' data-slide-to='5'></li>
+				  </ol>
+				  <!-- Carousel items -->
+				  <div class='carousel-inner'>
+				    <div class='item'>
+                        <div class='profile-circle' style='background-color: rgba(0,0,0,.2);'></div>
+				    	<blockquote>
+				    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, veritatis nulla eum laudantium totam tempore optio doloremque laboriosam quas, quos eaque molestias odio aut eius animi. Impedit temporibus nisi accusamus.</p>
+				    	</blockquote>	
+				    </div>
+				    <div class='item'>
+                        <div class='profile-circle' style='background-color: rgba(77,5,51,.2);'></div>
+				    	<blockquote>
+				    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, veritatis nulla eum laudantium totam tempore optio doloremque laboriosam quas, quos eaque molestias odio aut eius animi. Impedit temporibus nisi accusamus.</p>
+				    	</blockquote>
+				    </div>
+				    <div class='active item'>
+                        <div class='profile-circle' style='background-color: rgba(145,169,216,.2);'></div>
+				    	<blockquote>
+				    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, veritatis nulla eum laudantium totam tempore optio doloremque laboriosam quas, quos eaque molestias odio aut eius animi. Impedit temporibus nisi accusamus.</p>
+				    	</blockquote>
+				    </div>
+                    <div class='item'>
+                        <div class='profile-circle' style='background-color: rgba(77,5,51,.2);'></div>
+    			    	<blockquote>
+				    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, veritatis nulla eum laudantium totam tempore optio doloremque laboriosam quas, quos eaque molestias odio aut eius animi. Impedit temporibus nisi accusamus.</p>
+				    	</blockquote>
+				    </div>
+                    <div class='item'>
+                        <div class='profile-circle' style='background-color: rgba(77,5,51,.2);'></div>
+    			    	<blockquote>
+				    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, veritatis nulla eum laudantium totam tempore optio doloremque laboriosam quas, quos eaque molestias odio aut eius animi. Impedit temporibus nisi accusamus.</p>
+				    	</blockquote>
+				    </div>
+                    <div class='item'>
+                        <div class='profile-circle' style='background-color: rgba(77,5,51,.2);'></div>
+    			    	<blockquote>
+				    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, veritatis nulla eum laudantium totam tempore optio doloremque laboriosam quas, quos eaque molestias odio aut eius animi. Impedit temporibus nisi accusamus.</p>
+				    	</blockquote>
+				    </div>
+				  </div>
+				</div>
+			</div>							
+		</div>
+	</div>
+</div>		
+</section>";
+include 'footer.php';
+echo "<!-- jQuery --><script src='js/jquery.js'></script><!-- Bootstrap Core JavaScript --><script src='js/bootstrap.min.js'></script><!-- Plugin JavaScript --><script src='js/jquery.easing.min.js'></script><script src='js/jquery.fittext.js'></script><script src='js/wow.min.js'></script><!-- Custom Theme JavaScript --><script src='js/creative.js'></script>
+</body>
+</html>";
+?>
