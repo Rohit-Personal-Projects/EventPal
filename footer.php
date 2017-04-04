412f6b6291fab94494d40f961283b76195a837b2
Vipul Munot <vipmunot@gmail.com>
2017-04-04 14:36:31 -0400

    corrected footer

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@ echo "<footer class='modal-footer'><!-- Social Section
 </div>
 </div>
 </footer>
-<!-- jQuery --><script src="js/jquery.js"></script><!-- Bootstrap Core JavaScript --><script src="js/bootstrap.min.js"></script><!-- Plugin JavaScript --><script src="js/jquery.easing.min.js"></script><script src="js/jquery.fittext.js"></script><script src="js/wow.min.js"></script><!-- Custom Theme JavaScript --><script src="js/creative.js"></script>
+<!-- jQuery --><script src='js/jquery.js'></script><!-- Bootstrap Core JavaScript --><script src='js/bootstrap.min.js'></script><!-- Plugin JavaScript --><script src='js/jquery.easing.min.js'></script><script src='js/jquery.fittext.js'></script><script src='js/wow.min.js'></script><!-- Custom Theme JavaScript --><script src='js/creative.js'></script>
 </body>
 </html>";
 ?>
