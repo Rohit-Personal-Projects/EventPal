94eb09e3d5d8e7b9f3f8af17052f5e41ab453e31
Rohit <rohitnair987@gmail.com>
2017-04-05 02:38:04 -0400

    Introducing models

---
+++ Models/Event.php
@@ -0,0 +1,19 @@
+<?php
+
+	include 'Location.php';
+
+	class Event {
+		var $EventId;
+		var $Organizer;
+		var $Title;
+		var $Description;
+		var $Days;
+		var $StartDate;
+		var $EndDate;
+		var $StartTime;
+		var $EndTime;
+		var $Image;
+		var $Location;
+	}
+
+?>
---
+++ Models/Location.php
@@ -0,0 +1,11 @@
+<?php
+
+	class Location {
+		var $Steet;
+		var $City;
+		var $Zip;
+		var $State;
+		var $Country; 
+	}
+	
+?>
