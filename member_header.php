5524004597037889821240c98e14fb30ce55f577
Vipul Munot <vipmunot@gmail.com>
2017-04-10 21:39:29 -0400

    completed members page

---
+++ member_header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+	<meta content="IE=edge" http-equiv="X-UA-Compatible" />
+	<meta content="width=device-width, initial-scale=1" name="viewport" />
+	<meta content="This is my one page resume website." name="description" />
+	<meta content="Vipul,Vipul Munot,Munot,Indiana University,Indiana, Indiana University Bloomington,Bloomington,Data Science, Data, Scientist,Data Scientist" name="keywords" />
+	<meta content="Vipul Munot" name="author" />
+	<title>Welcome | Eventpal</title>
+	<!-- Bootstrap Core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" /><!-- Custom Fonts -->
+	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" /><!-- Plugin CSS -->
+	<link href="css/animate.min.css" rel="stylesheet" type="text/css" /><!-- Custom CSS -->
+	<script src="js/modernizr.js"></script>
+	<link href="css/style.css" rel="stylesheet" /><!-- Fonts -->
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries --><!-- WARNING: Respond.js doesn"t work if you view the page via file:// --><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<header>
+    <!-- Fixed navbar -->
+  <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand animated bounceInLeft" href="index.php">Eventpal</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Search</a></li>
+            <li><a href="#about">Create Event</a></li>
+            <li><a href="">My Settings</a></li>
+            <li><a href="">My Events</a></li>            
+			<li><a href="">Logout</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+  </nav>
+</header>
---
+++ members.php
@@ -0,0 +1,116 @@
+<?php
+require 'member_header.php';
+?>
+
+<section id = "members">
+<div class = "container">
+<div class="clearfix">&nbsp;</div><div class="clearfix">&nbsp;</div><div class="clearfix">&nbsp;</div>
+<div class ="col-xs-12 col-sm-6 col-lg-8">
+	<div class="row">
+	<h3>Welcome back!</h3><h4>Alien</h4>
+	<div class="row">
+	<h4>Today's Registered Events</h4>
+	</div><!--/row-->
+	<div class="row">
+		<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+		<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+			<h3>Adventure</h3>
+			<p><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;April 25,2017</p>
+			<p><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;4:00 - 5:00 PM</p>
+		<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+		</div>
+		<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+		<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+			<h3>Adventure</h3>
+			<p><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;April 25,2017</p>
+			<p><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;4:00 - 5:00 PM</p>
+		<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+		</div>
+		<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+		<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+			<h3>Adventure</h3>
+			<p><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;April 25,2017</p>
+			<p><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;4:00 - 5:00 PM</p>
+		<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+		</div>
+		<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+		<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+			<h3>Adventure</h3>
+			<p><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;April 25,2017</p>
+			<p><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;4:00 - 5:00 PM</p>
+		<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+		</div>
+		<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+		<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+			<h3>Adventure</h3>
+			<p><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;April 25,2017</p>
+			<p><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;4:00 - 5:00 PM</p>
+		<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+		</div>
+		<div class='col-md-4 col-sm-10 col-xs-11 wow bounceIn'>
+		<figure class='effect'><img alt='LMB Productions' src='images/bat.jpg' /> <figcaption>
+			<h3>Adventure</h3>
+			<p><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;April 25,2017</p>
+			<p><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;4:00 - 5:00 PM</p>
+		<a href='http://lmbproductions.in' target='_blank'>View more</a> <span class='icon'> </span> </figcaption></figure>
+		</div>
+		
+		
+		
+		
+	</div><!--/row-->
+
+
+	</div><!--/row-->
+</div><!--/col-8-->
+<div class ="col-xs-6 col-lg-4">
+	<div class="row">
+		<img src="images/calendar.jpg"/>
+	</div>
+	<div class="row">
+		<h4>Upcoming Events</h4>
+		<div class="row">
+			<ul type="none">
+			<li><h3><a href="">Alien Go Karting</a></h3></li>
+			<li><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;April 25,2017</li>
+			<li><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;4:00 - 5:00 PM</li>
+			<li><button class="btn btn-primary" type="submit" name="submit">Go for it!</button></li>
+			</ul>
+		</div><!--/row-->
+		<div class="row">
+			<ul type="none">
+			<li><h3><a href="">Alien Go Karting</a></h3></li>
+			<li><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;April 25,2017</li>
+			<li><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;4:00 - 5:00 PM</li>
+			<li><button class="btn btn-primary" type="submit" name="submit">Go for it!</button></li>
+			</ul>
+		</div><!--/row-->
+		<div class="row">
+			<ul type="none">
+			<li><h3><a href="">Alien Go Karting</a></h3></li>
+			<li><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;April 25,2017</li>
+			<li><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;4:00 - 5:00 PM</li>
+			<li><button class="btn btn-primary" type="submit" name="submit">Go for it!</button></li>
+			</ul>
+		</div><!--/row-->
+		<div class="row">
+			<ul type="none">
+			<li><h3><a href="">Alien Go Karting</a></h3></li>
+			<li><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;April 25,2017</li>
+			<li><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;4:00 - 5:00 PM</li>
+			<li><button class="btn btn-primary" type="submit" name="submit">Go for it!</button></li>
+			</ul>
+		</div><!--/row-->
+		<div class="row">
+			<ul type="none">
+			<li><h3><a href="">Alien Go Karting</a></h3></li>
+			<li><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;April 25,2017</li>
+			<li><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;4:00 - 5:00 PM</li>
+			<li><button class="btn btn-primary" type="submit" name="submit">Go for it!</button></li>
+			</ul>
+		</div><!--/row-->								
+				
+	</div><!--/row-->
+</div>
+</div><!--/container-->
+</section>
