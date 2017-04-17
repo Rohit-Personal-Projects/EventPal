af349009b1cd36ad75828b7d56dbef83308b87d0
Vipul Munot <vipmunot@gmail.com>
2017-04-17 15:19:04 -0400

    creating contact and faq

---
+++ contact.php
@@ -0,0 +1,8 @@
+<?php 
+require 'header.php';
+?>
+<section id = "contactus">
+</section>
+<?php 
+require 'footer.php';
+?>
---
+++ faqs.php
@@ -0,0 +1,8 @@
+<?php 
+require 'header.php';
+?>
+<section id = "faqs">
+</section>
+<?php 
+require 'footer.php';
+?>
