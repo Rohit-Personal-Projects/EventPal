a764ffa74f3f559b6c090e78968d6595a6835161
Rohit <rohitnair987@gmail.com>
2017-04-21 08:45:19 -0400

    event deregister

--- js/event.js
+++ js/event.js
@@ -1,3 +1,11 @@
+function initEventRegisterDisplays() {
+	document.getElementById("eventRegisterBtnSuccess").style.display= 'block';
+	document.getElementById("eventRegisterBtnFailure").style.display= 'none';
+	document.getElementById("eventRegisterBtn").style.display= 'none';
+}
+
+
+
 function eventRegister(memberId, eventId) {
 	data =  {
     	'name': 'insertEventRegister',
@@ -23,8 +31,32 @@ function eventRegister(memberId, eventId) {
 
 }
 
-function initEventRegisterDisplays() {
-	document.getElementById("eventRegisterBtnSuccess").style.display= 'block';
-	document.getElementById("eventRegisterBtnFailure").style.display= 'none';
-	document.getElementById("eventRegisterBtn").style.display= 'none';
+
+
+function eventDeRegister(memberId, eventId) {
+	data =  {
+    	'name': 'removeEventRegister',
+    	'memberId': memberId,
+    	'eventId': eventId
+    };
+
+    $.post(
+    	'ajax.php', 
+    	data, 
+    	function (response) {
+	        if(response) {
+	        	document.getElementById("eventRegisterBtnSuccess").style.display= 'none';
+	        	document.getElementById("eventRegisterBtnFailure").style.display= 'none';
+	        	document.getElementById("eventRegisterBtn").style.display= 'block';
+	        }
+	        else {
+	        	document.getElementById("eventRegisterBtnSuccess").style.display= 'none';
+	        	document.getElementById("eventRegisterBtnFailure").style.display= 'none';
+	        }
+    	}
+    );
+
 }
+
+
+
