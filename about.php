d7c85971b9cac27d4c515487ee08f40f7503daf0
Rohit <rohitnair987@gmail.com>
2017-04-21 18:49:59 -0400

    put in my stuff

--- about.php
+++ about.php
@@ -1,58 +1,102 @@
 <?php 
-require 'header.php';
+  require 'header.php';
 ?>
-<section id = "aboutus">
-<div class = "clearfix">&nbsp;</div><div class = "clearfix">&nbsp;</div>
-<div class="container"><div class="row"><div class="col-md-12">
-			<h1>About Us </h1>
-			<h2>What and Who is EventPal?</h2>
-		
-		
-					<p class="text-justify"></strong>EventPal is the biggest friendship social networking sites available. Established in April, 2017</a>, EventPal is growing at an amazing pace. Free for all to sign up, EventPal website allows them to talk, share and bond in whatever ways they are comfortable. <br /><br />Events are posted by a local rep and sometimes local companies. The allows people to find friends for various activities on topics ranging from the best local hair salons to how best to get over a break up or how to deal with fleas on your pet. <br />EventPal brings people together in thousands of cities to do more of what they want to do in life. It is organized around one simple idea: <strong>when we get together and do the things that matter to us, we’re at our best.</strong></p><br /><br />
-					
-					<article id="faq-wrapper">
 
-<h1>Meet The EventPal Team</h1> 
-  <h2>We are the place where people of all ages and backgrounds can come to talk, share and bond with new friends. </h2> <br />
-  
- 
-<h3>Our Team</h3>
-  <table width="100%" border="0" cellpadding="0">
-        <tr>
-      <td valign="top"><p><img src="S:\Info Arc Website\finalwebsite\EventPal-master\profpic.jpg" alt="Amanda Blain" hspace="5" vspace="5" border="1" /></p></td>
-      <td valign="top"><p class="text-justify"><strong>Rohit Nair</strong> amanda (at) girlfriendsocial.com - <a href="http://www.amandablain.com">AmandaBlain.com</a>  <br />
-      Amanda is the driving force behind Girlfriend Social. She currently lives in Toronto, Ontario, Canada. She's the one that has designed the concept, website, content and graphics for the site. With an ever-growing base of followers across all social media channels, including well over 2 million people having circled her on Google+, she is a tour-de-force in many technical areas, and social areas.<br>
- </p>
-      </td>
-  </tr>
-    <tr>
-      <td valign="top"><p><img src="S:\Info Arc Website\finalwebsite\EventPal-master\profpic.jpg" alt="Amanda Blain" hspace="5" vspace="5" border="1" /></p></td>
-      <td valign="top"><p class="text-justify"><strong>Shruthi Ramakrishnan</strong> amanda (at) girlfriendsocial.com - <a href="http://www.amandablain.com">AmandaBlain.com</a>  <br />
-      Amanda is the driving force behind Girlfriend Social. She currently lives in Toronto, Ontario, Canada. She's the one that has designed the concept, website, content and graphics for the site. With an ever-growing base of followers across all social media channels, including well over 2 million people having circled her on Google+, she is a tour-de-force in many technical areas, and social areas.<br>
- </p>
-      </td>
-  </tr>
-      <tr>
-      <td valign="top"><p><img src="S:\Info Arc Website\finalwebsite\EventPal-master\profpic.jpg" alt="Amanda Blain" hspace="5" vspace="5" border="1" /></p></td>
-      <td valign="top"><p class="text-justify"><strong>Vipul Munot</strong> amanda (at) girlfriendsocial.com - <a href="http://www.amandablain.com">AmandaBlain.com</a>  <br />
-      Amanda is the driving force behind Girlfriend Social. She currently lives in Toronto, Ontario, Canada. She's the one that has designed the concept, website, content and graphics for the site. With an ever-growing base of followers across all social media channels, including well over 2 million people having circled her on Google+, she is a tour-de-force in many technical areas, and social areas.<br>
- </p>
-      </td>
-  </tr>
-</table>
- 
-<h3>Our Local Commitment</h3>
-<p class="text-justify">EventPal Social, based in Bloomington, Indiana, USA is currently available for everyone within the USA. What are you waiting for? <a href="./signup.php">Sign up now!</a></p>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-          </div>
+<section id = "aboutus">
+  <div class = "clearfix">&nbsp;</div>
+  <div class = "clearfix">&nbsp;</div>
+  <div class="container"><div class="row">
+    <div class="col-md-12">
+  		<h1 align=center>About Us </h1>
+  		<h2>What and Who is EventPal?</h2>
+  		
+  		<p class="text-justify"><strong>We are the place where people of all ages and backgrounds can come to talk, share and bond with new friends. </strong>EventPal is the biggest friendship social networking sites available. Established in April, 2017</a>, EventPal is growing at an amazing pace. Free for all to sign up, EventPal website allows them to talk, share and bond in whatever ways they are comfortable. <br /><br />Events are posted by a local rep and sometimes local companies. The allows people to find friends for various activities on topics ranging from the best local hair salons to how best to get over a break up or how to deal with fleas on your pet. <br />EventPal brings people together in thousands of cities to do more of what they want to do in life. It is organized around one simple idea: <br><h4><strong>&nbsp; &nbsp; &nbsp; "When we get together and do the things that matter to us, we’re at our best."</strong></h4></p><br><br>
+  		
+  		<article id="faq-wrapper">
+
+      <br> 
+      <h1 align=center>Our Team</h1>
+
+      <table width="100%" border="0" cellpadding="0">
+          
+          <!-- Rohit -->
+          <tr>
+            <td valign="top">
+            <p>
+              <a href = "/member.php?memberid=1">
+                <img src="Images/Admins/Rohit.jpg" alt="Rohit Nair" hspace="5" vspace="5" border="1" style="max-width: 100px; max-height: 100px;"/>
+              </a>
+            </p>
+            </td>
+
+            <td valign="center">
+              <p class="text-justify">
+                <a href = "/member.php?memberid=1"><strong>Rohit Nair</strong></a>
+                - 
+                <a href="mailto:RohitNair987@gmail.com">RohitNair987@gmail.com</a>  
+                <br>
+                Software Developer/Problem solver. I constantly look for new challenges that keep me on the edge.
+                <br>
+              </p>
+            </td>
+          </tr>
+
+
+          <!-- Shruthi -->
+          <tr>
+            <td valign="top">
+            <p>
+              <a href = "/member.php?memberid=1">
+                <img src="Images/Admins/Shruthi.jpg" alt="Shruthi" hspace="5" vspace="5" border="1" style="max-width: 100px; max-height: 100px;"/>
+              </a>
+            </p>
+            </td>
+
+            <td valign="center">
+              <p class="text-justify">
+                <a href = "/member.php?memberid=1"><strong>Shruthi</strong></a>
+                - 
+                <a href="mailto:email@gmail.com">email@gmail.com</a>  
+                <br>
+                Shruthi's one liner
+                <br>
+              </p>
+            </td>
+          </tr>
+
+
+          <!-- Vipul -->
+          <tr>
+            <td valign="top">
+            <p>
+              <a href = "/member.php?memberid=1">
+                <img src="Images/Admins/Vipul.jpg" alt="Vipul" hspace="5" vspace="5" border="1" style="max-width: 100px; max-height: 100px;"/>
+              </a>
+            </p>
+            </td>
+
+            <td valign="center">
+              <p class="text-justify">
+                <a href = "/member.php?memberid=1"><strong>Vipul</strong></a>
+                - 
+                <a href="mailto:email@gmail.com">email@gmail.com</a>  
+                <br>
+                Alien's one liner
+                <br>
+              </p>
+            </td>
+          </tr>
+
+      </table>
+
+      <br> 
+      <h1 align=center>Our Local Commitment</h1>
+      <p class="text-justify">EventPal, based in Bloomington, Indiana, USA is currently available for everyone within the USA. What are you waiting for? <a href="./register.php">Sign up now!</a></p>
+
+    </div>
   </div>
 </section>
+
 <?php 
-require 'footer.php';
+  require 'footer.php';
 ?>
