561e3131d94bbecc625a9931746ec620e354be08
Vipul Munot <vipmunot@gmail.com>
2017-04-21 20:27:35 -0400

    updating footer

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
    ================================================== -->
 <div class="row">
 <div class="social-container">
+
             <ul class="social">
                <li><a href="https://facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
                <li><a href="https://www.linkedin.com" target="_blank"><i class="fa fa-linkedin"></i></a></li>
@@ -10,7 +11,11 @@
                <li><a href="http://snapchat.com" target="_blank"><i class="fa fa-snapchat-ghost"></i></a></li>               
             </ul>
 </div>
+
 </div>
+	<div class = "row social-container">
+   <p><a href = "about.php" target="_blank">About us</a> | <a href = "contact.php" target="_blank">Contact us</a> | <a href = "faqs.php" target="_blank">FAQ's</a></p>
+   <p>&copy; All Rights Reserved by Vipul Munot, Rohit Nair and Shruthi Ramakrishnan.</p></div>
 </footer>
 <!-- jQuery --><script src="js/jquery.js"></script><!-- Bootstrap Core JavaScript --><script src="js/bootstrap.min.js"></script><!-- Plugin JavaScript --><script src="js/jquery.easing.min.js"></script><script src="js/jquery.fittext.js"></script><script src="js/wow.min.js"></script><!-- Custom Theme JavaScript --><script src="js/creative.js"></script>
 </body>
