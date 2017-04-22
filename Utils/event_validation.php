622973384d60a36750c4b47c04336dac42958034
Vipul Munot <vipmunot@gmail.com>
2017-04-22 01:04:47 -0400

    updating image verification

--- Utils/event_validation.php
+++ Utils/event_validation.php
@@ -18,9 +18,7 @@ $dayofweek = array();
 $catint = array();
 
 $target_dir = "../Images/Events/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
 
 
@@ -32,7 +30,50 @@ $conn = mysqli_connect(SERVER_NAME, USER_NAME, PASSWORD, DATABASE_NAME);
 		
 
 if(isset($_POST['create_submit'])){
+	//Images
 
+
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
+
+	$tmp_file = str_replace("../", "", $target_file);
 	
 
 
@@ -177,7 +218,7 @@ if(isset($_POST['create_submit'])){
 
 	if(empty($error_array)) {
 
-		$query = mysqli_query($conn, "insert into Event(OrganizerId,Title,Description,Days,StartDate,EndDate,StartTime,EndTime,Street,City,Zip,State,Country) values ('$memid','$ename','$desc','$dayofweek','$stdate','$endate','$sttime','$entime','$address','$city','$zip','$state','$country')");		
+		$query = mysqli_query($conn, "insert into Event(OrganizerId,Title,Description,Days,StartDate,EndDate,StartTime,EndTime,Street,City,Zip,State,Country,Image) values ('$memid','$ename','$desc','$dayofweek','$stdate','$endate','$sttime','$entime','$address','$city','$zip','$state','$country','$tmp_file')");		
 	
 	array_push($error_array, "<span style='color: #14C800;'>You're all set! Go ahead and login!</span><br>");	
 	header("Location: members.php");
