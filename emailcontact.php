fecf5e9df1ef026d7c5ecdc6be6c6faeb8871cfb
Vipul Munot <vipmunot@gmail.com>
2017-04-21 21:54:27 -0400

    final touches to email

--- emailcontact.php
+++ emailcontact.php
@@ -4,8 +4,8 @@ $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
 $mail_to = "vipmunot@gmail.com";
-$subject = $name . "from Eventpal website Reaching Out";
-$email_msg = $name . " is reaching out on behalf of eventpal. The person has send following message for you:\n" . $message;
+$subject = $name . " from Eventpal website Reaching Out";
+$email_msg = $name . " is reaching out on behalf of eventpal. The person is email is:" . $visitor_email . " The person has send following message for you:\n" . $message;
 mail($mail_to,$subject,$email_msg);
 $a =  "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
 echo "<script type='text/javascript'>alert('$a');</script>";
