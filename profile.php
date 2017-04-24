50faba845dc811895d2809aa26f5949d2a3c5387
Vipul Munot <vipmunot@gmail.com>
2017-04-24 17:56:38 -0400

    made some changes in Database and Profile Page

--- profile.php
+++ profile.php
@@ -18,7 +18,7 @@
 		<div class="clearfix">&nbsp;</div>
 		<div class ="col-xs-4 col-lg-3">
 		<div class = "row">
-		<img alt="Member Image" src = "Images/Members/Vipul.jpg" class="img-responsive center-block" />
+		<img alt="Member Image" src = "Images/Members/ali.jpg" class="img-responsive center-block" />
 		</div><!--/row-->
 
 		<div class = "row text-center">
@@ -35,9 +35,9 @@
 		<div class ="col-xs-12 col-sm-6 col-lg-8">
 		
 		<div class = "row">
-		<h3>First Name Last Name</h3>
-		<p><i class="fa fa-map-marker">&nbsp;City Name, Country</i></p>
-		<p class="text-justify">Bio. Jus the admin of the site. Haha</p>
+		<h3>Ali Ghazinejad</h3>
+		<p><i class="fa fa-map-marker">&nbsp;Bloomington, US</i></p>
+		<p class="text-justify">I am a PhD candidate in information science (minoring in cognitive science) at Indiana University Bloomington. Among other things, I am interested in blending theoretical knowledge with social behavioral data to understand why groups of people behave the way they do. My toolbox usually includes a variety of computational tools such as agent-based modeling, machine learning, visualizations and so on.I was born and raised in Iran a few decades ago and speak Persian (Farsi).</p>
 		</div><!--/row-->
 				
 
